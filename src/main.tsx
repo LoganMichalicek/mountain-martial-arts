3b3322fd5412caee951ab0d79a793593f7e6392b
Logan Michalicek <logan.michalicek@gmail.com>
2023-05-02 18:33:40 -0600

    Removed styled-components implementation

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
-import * as themes from './themes/schema.json';
-import { setToLS } from './utils/localStorage.ts';
 
 const clerkPubKey = import.meta.env.VITE_DEV_REACT_APP_CLERK_PUBLISHABLE_KEY;
-
-const Index = () => {
-  setToLS('all-themes', JSON.stringify(themes));
-  return (
-    <App />
-  );
-}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkPubKey}>
-      <Index />
+      <App />
     </ClerkProvider>
   </React.StrictMode>,
 )
